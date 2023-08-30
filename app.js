a2a67da0d3a4b3d46b192b575f80df49e20ca37e
felipehmora <felipehmoraarg@gmail.com>
2023-08-30 19:33:45 -0400

    levantando servidor

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+const http = require("http")
 const express = require("express");
 const app = express();
+const io = require('socket.io');
 
-app.use(express.static(__dirname + "/public"))
+const server = http.createServer(app);
+app.set("port", 3000);
+app.use(express.static(__dirname + "/public"));
 
-app.listen(3000, function () {
-    console.log("el servidor se ha iniciado")
+server.listen(app.get("port"), function () {
+    console.log("el servidor se ha iniciado");
+})
+
+let socket = io(server);
+
+socket.on('connect', function(socket){
+    console.log("nuevo cliente conectado")
 })
--- public/main.js
+++ public/main.js
@@ -1,14 +1,15 @@
 $(function () {
+    //socket iniciado
+    let socket = io()
+
     //variables
     let message = $("#chat-message");
     let chat = $("#chat");
 
 
-    $message.focus()
+    message.focus()
     $("#message-box").submit(function (e){
         e.preventDefault();
         chat.append(message.val() + "<br>")
-
-
     })
 })
